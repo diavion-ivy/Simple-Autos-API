0d6954c1486c73b280b43eaadce8b83595068cd4
Ivy <diavion.ivy.vacc9o@statefarm.com>
2024-07-22 15:33:28 -0400

    get all autos test

---
+++ src/main/java/com/galvanize/simple_autos/AutosService.java
@@ -0,0 +1,10 @@
+package com.galvanize.simple_autos;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutosService {
+    public AutosList getAutos() {
+        return null;
+    };
+}
--- src/test/java/com/galvanize/simple_autos/AutoControllerTests.java
+++ src/test/java/com/galvanize/simple_autos/AutoControllerTests.java
@@ -1,8 +1,47 @@
 package com.galvanize.simple_autos;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AutosController.class)
 public class AutoControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    AutosService autosService;
+
+
     //GET: /api/autos
     // returns list of all autos
+    @Test
+    void getAutos_noParam_exist_returnsAutoslists() throws Exception {
+        List<Automobile> automobiles = new ArrayList<>();
+        for (int i = 0; i < 5 ; i++) {
+            automobiles.add(new Automobile(1900+i, "Ford", "Mustang","AA88"+i));
+        }
+        when(autosService.getAutos()).thenReturn(new AutosList(automobiles));
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/autos"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.automobiles", hasSize(5)));
+    }
+
+
     // returns 204 no autos found
     // /api/autos?color=RED returns red cars
     // /api/autos?make=Ford returns ford cars
