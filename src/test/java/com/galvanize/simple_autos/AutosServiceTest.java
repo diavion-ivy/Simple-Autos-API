09a6c84b017da9f38beaa9137b3f796e5ec18bf3
Ivy <diavion.ivy.vacc9o@statefarm.com>
2024-07-23 15:15:03 -0400

    getAutos service test

---
+++ src/test/java/com/galvanize/simple_autos/AutosServiceTest.java
@@ -0,0 +1,50 @@
+package com.galvanize.simple_autos;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class AutosServiceTest {
+
+    private AutosService autosService;
+
+    @Mock
+    AutosRepository autosRepository;
+
+    @BeforeEach
+    void setUp() {
+        autosService = new AutosService(autosRepository);
+    }
+
+    @Test
+    void getAutos() {
+        AutosList autosList = autosService.getAutos();
+        assertThat(autosList).isNotNull();
+    }
+
+    @Test
+    void testGetAutos() {
+    }
+
+    @Test
+    void addAuto() {
+    }
+
+    @Test
+    void getAuto() {
+    }
+
+    @Test
+    void updateAuto() {
+    }
+
+    @Test
+    void deleteAuto() {
+    }
+}
