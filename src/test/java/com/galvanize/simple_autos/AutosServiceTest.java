f941ffa1584881cc7b0470d178fe943a4bd5624d
Ivy <diavion.ivy.vacc9o@statefarm.com>
2024-07-23 15:50:45 -0400

    getAuto_withVin_returnsAuto service test

--- src/test/java/com/galvanize/simple_autos/AutosServiceTest.java
+++ src/test/java/com/galvanize/simple_autos/AutosServiceTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -54,7 +55,14 @@ class AutosServiceTest {
     }
 
     @Test
-    void getAuto() {
+    void getAuto_withVin_returnsAuto() {
+        Automobile automobile = new Automobile(1967, "Mustang", "Ford","AA88CC");
+        automobile.setColor("RED");
+        when(autosRepository.findByVin(anyString()))
+                .thenReturn(Optional.of(automobile));
+        Automobile auto = autosService.getAuto(automobile.getVin());
+        assertThat(auto).isNotNull();
+        assertThat(auto.getVin()).isEqualTo(automobile.getVin());
     }
 
     @Test
