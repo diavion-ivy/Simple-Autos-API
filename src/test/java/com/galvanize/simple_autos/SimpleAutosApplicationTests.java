d04cfea29cb02e67cf4e9f85fa7112d3933d2953
Ivy <diavion.ivy.vacc9o@statefarm.com>
2024-07-23 16:54:59 -0400

    SimpleAutosApplicationTests comments

--- src/test/java/com/galvanize/simple_autos/SimpleAutosApplicationTests.java
+++ src/test/java/com/galvanize/simple_autos/SimpleAutosApplicationTests.java
@@ -1,12 +1,26 @@
 package com.galvanize.simple_autos;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SimpleAutosApplicationTests {
 
-//	@Test
+//	@Autowired
+//	TestRestTemplate restTemplate;
+//
+//	@Autowired
+//	AutosRepository autosRepository;
+//
+//	@BeforeEach
+//	void setUp() {
+//
+//	}
+
+	//	@Test
 //	void contextLoads() {
 //	}
 
